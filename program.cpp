7d95e19cbffb58ff022f3249d2ced71414d10e46
Emilia Flisikowska <emilusiafli@wp.pl>
2024-11-05 23:52:13 +0100

    Added factorial to program.cpp

--- program.cpp
+++ program.cpp
@@ -2,9 +2,15 @@
 
 using namespace std;
 
+int factorial(int n){
+	if(n<=1)
+		return 1;
+	return n*factorial(n-1);
+}
+
 int main(){
 	int n;
 	cin>>n;
-	cout<<n<<endl;
+	cout<<factorial(n)<<endl;
 	return 0;
 }
